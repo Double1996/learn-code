1c89de19231ac243458a9fc948d1b3820c09b4db
double2 <forrestshuang@gmail.com>
2021-03-20 00:35:43 +0800

    插入排序

---
+++ golang/sort/insertion_sort.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nums = []int{1, 13, 23, 56, 99, 87, 23, 7, 103}
+
+	for i := 1; i < len(nums)-1; i++ {
+		cur := nums[i]
+		j := i - 1
+		for cur < nums[j] && j > 0 {
+			nums[j+1] = nums[j]
+			j--
+		}
+		nums[j+1] = cur
+	}
+	fmt.Println(nums)
+}
