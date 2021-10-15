e2848d19fe44eb427b13b490cb4cfeaf18752293
double2 <shaunglei@xiaoyouzi,com>
2021-10-15 14:57:38 +0800

    leetcode 三数之和

--- alg/case/three-nums.go
+++ alg/case/three-nums.go
@@ -1,16 +1,14 @@
-/*
- * @lc app=leetcode.cn id=15 lang=golang
- *
- * [15] 三数之和
- 时间复杂度：
- 空间复杂度: 
- */
+package main
 
-// @lc code=start
+import (
+	"fmt"
+	"sort"
+)
+
 func threeSum(nums []int) [][]int {
-	// 时间复杂度:
-	// 空间复杂度:
-	// 步骤  先排序， 左右两个指针
+	// 时间复杂度
+	// 空间复杂度
+	// 步骤
 
 	sort.Ints(nums) // 排序
 	var res [][]int
@@ -19,7 +17,7 @@ func threeSum(nums []int) [][]int {
 
 	for i := 0; i < l-2; i++ { // 双指针的选择方式
 		n1 := nums[i]
-		if n1 > 0 { // 边界条件
+		if n1 >= 0 { // 边界条件
 			break
 		}
 		if i > 0 && nums[i-1] == nums[i] { // 去重
@@ -36,8 +34,8 @@ func threeSum(nums []int) [][]int {
 			} else if n1+n2+n3 < 0 {
 				a++
 			} else {
-				res = append(res, []int{n1, n2, n3}) 
-				for a < b && nums[a] == n2 { // 双指针收缩
+				res = append(res, []int{i, a, b})
+				for a < b && nums[a] == n2 {
 					a++
 				}
 				for a < b && nums[b] == n3 {
@@ -48,5 +46,8 @@ func threeSum(nums []int) [][]int {
 	}
 	return res
 }
-// @lc code=end
 
+func main() {
+	res := threeSum([]int{-1, 0, 1, 2, -1, -4})
+	fmt.Println(res)
+}
