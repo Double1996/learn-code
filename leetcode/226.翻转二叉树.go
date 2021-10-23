b396aa40394db4f44543592b4b4fe8c36b176080
double2 <forrestshuang@gmail.com>
2021-10-23 16:10:27 +0800

    leetcode 翻转二叉树

---
+++ leetcode/226.翻转二叉树.go
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=226 lang=golang
+ *
+ * [226] 翻转二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func invertTree(root *TreeNode) *TreeNode {
+	// 思路
+	// 时间复杂度
+	// 空间复杂度
+
+	if root == nil {  //de
+		return root
+	}
+
+		// 交换左右两个节点
+		tmp := root.Left
+		root.Left = root.Right
+		root.Right = tmp
+		invertTree(root.Right)
+		invertTree(root.Left)
+		return root 
+}
+// @lc code=end
+
