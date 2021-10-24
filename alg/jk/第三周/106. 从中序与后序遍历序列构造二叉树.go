95bd0df17cdd2f5c3cbe7b32559ab5e17d686f7b
double2 <forrestshuang@gmail.com>
2021-10-24 17:38:00 +0800

    极客时间第三周

---
+++ alg/jk/第三周/106. 从中序与后序遍历序列构造二叉树.go
@@ -0,0 +1,22 @@
+package 第三周
+
+func buildTree(inorder []int, postorder []int) *TreeNode {
+	idxMap := map[int]int{}
+	for i, v := range inorder {
+		idxMap[v] = i
+	}
+	var build func(int, int) *TreeNode
+	build = func(inorderLeft, inorderRight int) *TreeNode {
+		if inorderLeft > inorderRight {
+			return nil
+		}
+		val := postorder[len(postorder)-1]
+		postorder = postorder[:len(postorder)-1]
+		root := &TreeNode{Val: val}
+		inorderRootIndex := idxMap[val]
+		root.Right = build(inorderRootIndex+1, inorderRight)
+		root.Left = build(inorderLeft, inorderRootIndex-1)
+		return root
+	}
+	return build(0, len(inorder)-1)
+}
