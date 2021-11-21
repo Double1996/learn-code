c709e09bae81e17a3ab23c0923d7d0674b8937b3
double2 <forrestshuang@gmail.com>
2021-11-21 22:57:09 +0800

    极客时间第7周

---
+++ alg/jk/第7周/279. 完全平方数.go
@@ -0,0 +1,32 @@
+package 第7周
+
+import "math"
+
+func isPerfectSquare(x int) bool {
+	y := int(math.Sqrt(float64(x)))
+	return y*y == x
+}
+
+// 判断是否能表示为 4^k*(8m+7)
+func checkAnswer4(x int) bool {
+	for x%4 == 0 {
+		x /= 4
+	}
+	return x%8 == 7
+}
+
+func numSquares(n int) int {
+	if isPerfectSquare(n) {
+		return 1
+	}
+	if checkAnswer4(n) {
+		return 4
+	}
+	for i := 1; i*i <= n; i++ {
+		j := n - i*i
+		if isPerfectSquare(j) {
+			return 2
+		}
+	}
+	return 3
+}
