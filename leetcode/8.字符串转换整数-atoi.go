46f0504fef91ac5b62929ff8da39cf3b62b3a959
double2 <forrestshuang@gmail.com>
2022-01-10 17:40:40 +0800

    atoi

--- leetcode/8.字符串转换整数-atoi.go
+++ leetcode/8.字符串转换整数-atoi.go
@@ -6,7 +6,38 @@
 
 // @lc code=start
 func myAtoi(s string) int {
+	index := 0 
+	// 1. 忽略空格
+	for index < len(s) && s[index] == ' ' {
+			index++
+	}
 
+
+	sign := 1 
+	// 2. 判断正负号
+	if index<len(s) && (s[index] == '+'||s[index] == '-'){
+        if s[index] == '+' {
+           sign = 1
+           
+        }else{
+            sign = -1
+            
+        }
+        index++
+  }
+	ans := 0
+	
+	for index< len(s) && s[index] >= '0' && s[index] <= '9' {
+		if ans > (2147483648 - int(s[index] - '0')) / 10  {
+				if sign == 1 {
+					return math.MaxInt32
+				}
+				return math.MinInt32
+		}
+		ans = ans*10 + int(s[index] - '0')
+		index++
+	} 
+	return ans * sign
 }
 // @lc code=end
 
