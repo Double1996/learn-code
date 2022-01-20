f6719d91ab5251a7e2b124c2b38923de83955262
double2 <forrestshuang@gmail.com>
2022-01-21 06:48:03 +0800

    for: 坑 同一块地址接受 range 中的值

--- golang/keyword/for.go
+++ golang/keyword/for.go
@@ -33,5 +33,18 @@ func for3() {
 }
 
 func main() {
-	for2()
+	slice := []int{0, 1, 2, 3}
+	myMap := make(map[int]*int)
+
+	for index, value := range slice {
+		myMap[index] = &value // 同一块内存去接收循环中的值
+	}
+	fmt.Println("=====new map=====")
+	prtMap(myMap)
+}
+
+func prtMap(myMap map[int]*int) {
+	for key, value := range myMap {
+		fmt.Printf("map[%v]=%v\n", key, *value)
+	}
 }
