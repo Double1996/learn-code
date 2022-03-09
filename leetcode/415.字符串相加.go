62df8e69dcc2624f350227f0f595d6c20f9a9687
double2 <forrestshuang@gmail.com>
2022-03-10 02:12:09 +0800

    添加 字符 注释

--- leetcode/415.字符串相加.go
+++ leetcode/415.字符串相加.go
@@ -10,12 +10,13 @@ func addStrings(num1 string, num2 string) string {
 	/*
 	我们定义两个指针 ii 和 jj 分别指向 \textit{num}_1num 
   和 \textit{num}_2num 
-  的末尾，即最低位，同时定义一个变量 \textit{add}add 维护当前是否有进位，然后从末尾到开头逐位相加即可。你可能会想两个数字位数不同怎么处理，这里我们统一在指针当前下标处于负数的时候返回 00，等价于对位数较短的数字进行了补零操作
+  的末尾，即最低位，同时定义一个变量 \textit{add}add 维护当前是否有进位，然后从末尾到开头逐位相加即可。
+	你可能会想两个数字位数不同怎么处理，这里我们统一在指针当前下标处于负数的时候返回 00，等价于对位数较短的数字进行了补零操作
 	*/
 	var res string
 	a, b, ca := len(num1) -1, len(num2) -1, 0 // 左右的指针，进位的
 	var n1, n2, sum int
-	for a >=0 || b >= 0 {
+	for a >=0 || b >= 0 {  // 从大到小的进行遍历，
 		n1, n2 = 0, 0
 		if a >= 0 {
 			 n1 = int(num1[a] - '0')
@@ -26,11 +27,11 @@ func addStrings(num1 string, num2 string) string {
 		// 该位权数和
 		sum =  n1 + n2 + ca
 		// 叠加进位
-		ca = sum / 10
+		ca = sum / 10  // 最高进位
 
 		// 每次的res都是逆序的，所以最后是正序
 		res = strconv.Itoa(sum%10) + res
-		a--
+		a--          // 递减的操作
 		b--
 	}
 	if ca > 0 {
--- leetcode/88.合并两个有序数组.go
+++ leetcode/88.合并两个有序数组.go
@@ -10,29 +10,49 @@ func merge(nums1 []int, m int, nums2 []int, n int)  {
 		// 时间复杂度  Olog(n)
 		// 空间复杂度  2n
 
-		var a,b int
-		sorted := make([]int, 0, m+n)
+		// var a,b int
+		// sorted := make([]int, 0, m+n)
 		
-		for  {
-			if a == m {
-				sorted = append(sorted, nums2[b:]...)
-				break
-			}
+		// for  {
+		// 	if a == m {
+		// 		sorted = append(sorted, nums2[b:]...)
+		// 		break
+		// 	}
 
-			if b == n {
-				sorted = append(sorted, nums1[a:]...)
-				break
-			}
+		// 	if b == n {
+		// 		sorted = append(sorted, nums1[a:]...)
+		// 		break
+		// 	}
 			
-			if nums1[a] < nums2[b] {
-				sorted= append(sorted, nums1[a])
-				a++
+		// 	if nums1[a] < nums2[b] {
+		// 		sorted= append(sorted, nums1[a])
+		// 		a++
+		// 	} else {
+		// 		sorted= append(sorted, nums2[b])
+		// 		b++			
+		// 	}
+		// }
+		// copy(nums1, sorted)
+
+		index := len(nums1) - 1
+		m = m -1
+		n = n -1
+		for m >= 0 && n >= 0 {	// 迭代遍历
+			num1 := nums1[m]
+			num2 := nums2[n]
+			if num1 > num2 {
+				nums1[index] = num1
+				m--
 			} else {
-				sorted= append(sorted, nums2[b])
-				b++			
-			}
+				nums1[index] = num2
+				n--
 		}
-		copy(nums1, sorted)
+			index--
+		}
+		if n >= 0 {  // m +n == nums1.Length // 最大
+				copy(nums1[0:n+1], nums2[0:n+1])  // copy 作用，把 nums2 后面的值 n+1 进行最后的替换
+		}
+
 }
 // @lc code=end
 
