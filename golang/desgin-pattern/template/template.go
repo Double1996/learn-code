4b1663257efe83905e5f56458304ef006cd40529
double2 <forrestshuang@gmail.com>
2022-06-27 00:54:15 +0800

    模板模式

---
+++ golang/desgin-pattern/template/template.go
@@ -0,0 +1,50 @@
+package template
+
+import "fmt"
+
+type ISMS interface {
+	send(content string, phone int) error
+}
+
+
+type sms struct {
+	ISMS
+}
+
+func (s *sms)Vaild(content string) error {
+	if len(content) > 63 {
+		return fmt.Errorf("content is too long")
+	}
+	return nil
+}
+
+// send 发送短信
+func (s *sms)Send(content string, phone int) error {
+	if err := s.Vaild(content); err != nil {
+		return err
+	}
+	
+	// 调用子类的方法发送短信
+	return s.send(content, phone)
+}
+
+
+// TelecomSms 走电信通道
+type TelecomSms struct {
+	*sms
+}
+
+// NewTelecomSms NewTelecomSms
+func NewTelecomSms() *TelecomSms {
+	tel := &TelecomSms{}
+	// 这里有点绕，是因为 go 没有继承，用嵌套结构体的方法进行模拟
+	// 这里将子类作为接口嵌入父类，就可以让父类的模板方法 Send 调用到子类的函数
+	// 实际使用中，我们并不会这么写，都是采用组合+接口的方式完成类似的功能
+	tel.sms = &sms{ISMS: tel}
+	return tel
+}
+
+func (tel *TelecomSms) send(content string, phone int)  error {
+	fmt.Println("send by telecom success")
+	return nil
+}
